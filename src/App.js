29aef00b4e9193695a976e4eea4a5f8ab1195d91
Farseena-M <farseenamoyan19@gmail.com>
2023-10-13 13:47:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react'
 
-function App() {
+/* const App = () => {
+  let name="Frsn" ,place="Vengara";
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div><h2>{name}</h2>
+    <h3>{place}</h3></div>
+  )
 }
 
-export default App;
+export default App */
+
+
+
+
+/* const App = () => {
+  return (
+    <div>
+      <h1>Welcome All</h1>
+      <p>sgfhjgjfgghdgfghffhfkfkkhdfgdrtsrstgkdjk</p>
+    </div>
+  )
+}
+
+export default App */
+
+
+
+
+/* export default class App extends Component {
+  render() {
+    return (
+      <div><h1>Welcome All</h1>
+      <p>esttfuugtrriugsdgsgshdghj</p></div>
+    )
+  }
+} */
+
+
+
+
+
+
+
+
+
+
+/* 
+ export default class App extends Component {
+  render() {
+    let name="Frsn",place="Malappuaram"
+    return (
+      <div><h2>{name}</h2>
+      <h4>{place}</h4>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ipsam, possimus enim est ad 
+        quasi officiis libero perspiciatis soluta aut quia quis obcaecati esse tempore quo necessitatibus laudantium dolore atque?</p>
+      </div>
+     
+   )
+  }
+}  */
+
+
+
+
+
+
+import Data from './component/data'
+
+const App = () => {
+  return (
+    <div><Data/></div>
+  )
+}
+
+export default App
+
---
+++ src/component/data.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Data extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hloooo</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse et praesentium ipsum, rem repudiandae incidunt nemo. 
+            Fugiat, consequatur? Ipsum necessitatibus architecto in magni repudiandae. Laudantium tempora eum hic suscipit unde.</p>
+      </div>
+    )
+  }
+}
